df656f28816b841841a2c18f6f9de776cd86cb34
SHHIKA <miaivalse@gmail.com>
2022-01-29 11:29:58 +0900

    プロジェクト ファイルを追加します。

---
+++ SH-MusicPlayer/Lib/Files/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SH_MusicPlayer.Lib.Files
+{
+    public class Data
+    {
+        public static List<string> load(string path)
+        {
+            return File.ReadAllLines(path).ToList();
+        }
+
+    }
+}
---
+++ SH-MusicPlayer/Program.cs
@@ -0,0 +1,23 @@
+﻿using SH_MusicPlayer.Lib.Files;
+using System;
+
+namespace SH_MusicPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(string.Join(", ", Data.load("D:\\test.txt")));
+
+            Player player = new Player("D:\\test.txt");
+
+            player.SetMusic("D:\\cover\\Claw My Heart.mp3");
+
+            player.Play();
+
+            Console.ReadKey();
+        }
+    }
+}
